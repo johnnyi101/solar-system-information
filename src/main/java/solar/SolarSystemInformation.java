4ae058224631889969a2e50be14f13fd878adebe
Johnny <johnnyi.london2019a@gmail.com>
2020-01-14 10:14:11 +0000

    Commit 2

--- src/main/java/solar/SolarSystemInformation.java
+++ src/main/java/solar/SolarSystemInformation.java
@@ -1,4 +1,42 @@
 package solar;
 
-public class SolarSystemInformation {
+public class SolarSystemInformation
+{
+private String userID;
+private String objectType;
+private String objectName;
+
+    public SolarSystemInformation(String userID)
+    {
+        this.userID = userID;
+        if(userID.length()!=6 && !userID.matches("/^[A-Z]{2}/")){
+            setObjectName("Not Allowed");
+            setObjectType("Not Allowed");
+        }
+
+
+    }
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getObjectType() {
+        return objectType;
+    }
+
+    public void setObjectType(String objectType) {
+        this.objectType = objectType;
+    }
+
+    public String getObjectName() {
+        return objectName;
+    }
+
+    public void setObjectName(String objectName) {
+        this.objectName = objectName;
+    }
 }
--- src/test/java/solar/SolarSystemInformationTest.java
+++ src/test/java/solar/SolarSystemInformationTest.java
@@ -1,4 +1,40 @@
 package solar;
 
-public class SolarSystemInformationTest {
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SolarSystemInformationTest
+{
+    @Test
+    public void user_id_has_to_correct_length_else_not_allowed()
+    {
+        //arrange
+        String input = "XX999";
+        String expectedObjectType = "Not Allowed";
+        String expectedObjectName = "Not Allowed";
+        SolarSystemInformation cut = new SolarSystemInformation(input);
+        //act
+        String resultOT =  cut.getObjectType();
+        String resultON = cut.getObjectName();
+        //assert
+        assertEquals(expectedObjectName, resultON);
+        assertEquals(expectedObjectType, resultOT);
+    }
+    @Test
+    public void user_id_first_two_letters_must_be_upper_case_alpha_characters()
+    {
+        //arrange
+        String input = "1X999";
+        String expectedObjectType = "Not Allowed";
+        String expectedObjectName = "Not Allowed";
+        SolarSystemInformation cut = new SolarSystemInformation(input);
+        //act
+        String resultOT =  cut.getObjectType();
+        String resultON = cut.getObjectName();
+        //assert
+        assertEquals(expectedObjectName, resultON);
+        assertEquals(expectedObjectType, resultOT);
+    }
+
+
 }
