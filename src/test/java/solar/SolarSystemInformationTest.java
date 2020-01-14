8373432d98cffde2d8ad251442a5a92670569591
Johnny <johnnyi.london2019a@gmail.com>
2020-01-14 11:28:55 +0000

    Commit 3

--- src/test/java/solar/SolarSystemInformationTest.java
+++ src/test/java/solar/SolarSystemInformationTest.java
@@ -36,5 +36,20 @@ public class SolarSystemInformationTest
         assertEquals(expectedObjectName, resultON);
         assertEquals(expectedObjectType, resultOT);
     }
+    @Test
+    public void id_with_four_numbers_at_end_is_accepted()
+    {
+        //arrange
+        String input = "AB1234";
+        String expectedObjectType = "Valid";
+        String expectedObjectName = "Valid";
+        SolarSystemInformation cut = new SolarSystemInformation(input);
+        //act
+        String resultOT = cut.getObjectType();
+        String resultON = cut.getObjectName();
+        //assert
+        assertEquals(expectedObjectName, resultON);
+        assertEquals(expectedObjectType, resultOT);
+    }
 
 }
